5e5761a5657db6eb44bec188d1d6dd5ce3739d58
bredele <olivier.wietrich@gmail.com>
2017-04-25 18:09:09 -0600

    should chunk html with placeholder

--- test/spec.js
+++ test/spec.js
@@ -11,9 +11,14 @@ test('should return an array of arguments', assert => {
   assert.equal(args instanceof Array, true)
 })
 
-test('should return an array of html chunks as first argument', assert => {
-  assert.plan(2)
+test('should return an array of html as first argument', assert => {
+  assert.plan(1)
   const args = html('<button>hello world!</button>')
-  assert.equal(args instanceof Array, true)
   assert.equal(args[0][0], '<button>hello world!</button>')
+})
+
+test('should chunk html if placeholder', assert => {
+  assert.plan(1)
+  const args = html('<button>${label}</button>')
+  assert.deepEqual(args[0], ['<button>','</button>'])
 })
