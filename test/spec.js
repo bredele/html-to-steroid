b54add377b4b7b2bd5ae79a17e6c297ee504112c
bredele <olivier.wietrich@gmail.com>
2017-04-25 18:07:27 -0600

    make first test more readable

--- test/spec.js
+++ test/spec.js
@@ -5,7 +5,13 @@
 const test = require('tape')
 const html = require('..')
 
-test('should return an array of arguments with a single chunk of html as first index', assert => {
+test('should return an array of arguments', assert => {
+  assert.plan(1)
+  const args = html('<button>hello world!</button>')
+  assert.equal(args instanceof Array, true)
+})
+
+test('should return an array of html chunks as first argument', assert => {
   assert.plan(2)
   const args = html('<button>hello world!</button>')
   assert.equal(args instanceof Array, true)
