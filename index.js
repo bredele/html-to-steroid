bd6c0fbfc8922c30e006788efd8427ad6dd4069f
bredele <olivier.wietrich@gmail.com>
2017-04-25 18:14:42 -0600

    chunk html with placeholder expressions

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
 
 module.exports = function(html) {
-  return [[html]]
+  let chunks = []
+  let start = 0
+  html.replace(/\$\{([^{}]*)\}/g, function(_, placeholder, idx) {
+    chunks.push(html.substring(start, idx))
+    start = idx + 3 + placeholder.length
+  })
+  chunks.push(html.substring(start, html.length))
+  return [chunks]
 }
